db69b8aa4ba23a14eac51cd43234e2f9df95246c
Robert Tanase <robert@crowdconnected.com>
2020-02-21 18:23:31 +0000

    Initial Commit

---
+++ Colocator Mapwize iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Colocator Mapwize iOS
+//
+//  Created by TCode on 21/02/2020.
+//  Copyright © 2020 CrowdConnected. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
